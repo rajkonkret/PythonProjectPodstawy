3344e945c956da4e7638f80bad2722dd8878de4d
radek <rajkonkret660@gmail.com>
2026-06-08 14:47:40 +0200

    [1 17] update typy_danych_2_lista.py.py

--- day1/typy_danych_2_lista.py
+++ day1/typy_danych_2_lista.py
@@ -68,3 +68,57 @@ print(lista[:])
 
 print(lista[-2:0])  # [] [4:0]
 print(lista[0:-2])  # ['Radek', 'Tomek', 'Marek', 'Zenek'] [0:4]
+
+# 0 do 14
+# range()
+lista_15 = list(range(15))
+print(lista_15)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[::2])  # [start:stop:krok], [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[::3])  # [0, 3, 6, 9, 12]
+
+print(lista_15[::-1])
+# [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+print(lista_15[::-2])  # [14, 12, 10, 8, 6, 4, 2, 0]
+
+print(list(range(0, 15, 2)))  # (start, stop, krok) [0, 2, 4, 6, 8, 10, 12, 14]
+# range(15) od 0 do 14
+# range(0, 15) od 0 do 14
+
+tablica = [[1, 2], [3, 4]]
+print(tablica)  # [[1, 2], [3, 4]]
+# numpy - tablice/macierze
+
+print(lista)
+# ['Radek', 'Tomek', 'Marek', 'Zenek', 'Karolina', 'Monika']
+
+# podmiana elementu
+lista[2] = "Sylwia"
+print(lista)
+# ['Radek', 'Tomek', 'Sylwia', 'Zenek', 'Karolina', 'Monika']
+
+# dopisanie w konkretne miejsce
+lista.insert(1, "Kamila")
+print(lista)
+# ['Radek', 'Kamila', 'Tomek', 'Sylwia', 'Zenek', 'Karolina', 'Monika']
+
+lista.append("Radek")
+print(lista)
+# ['Radek', 'Kamila', 'Tomek', 'Sylwia', 'Zenek', 'Karolina', 'Monika', 'Radek']
+
+# usunięcie elemntu, pierwszy z lewej
+lista.remove("Radek")
+print(lista)
+# ['Kamila', 'Tomek', 'Sylwia', 'Zenek', 'Karolina', 'Monika', 'Radek']
+
+# usunięcie po indeksie
+print(lista.pop(3))  # Zenek
+
+print(lista.pop(-1))  # Radek
+
+print(lista)
+print(lista.pop())  # Monika, usunie ostatni element
+
+# sprawdzenie indeksu elementu
+print(lista.index("Sylwia"))  # index numer 2

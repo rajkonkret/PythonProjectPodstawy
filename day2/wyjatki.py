5b395825e9107077469aff75881fac35b4c1506a
radek <rajkonkret660@gmail.com>
2026-06-09 13:30:22 +0200

    [2 20] update warunki2.py

---
+++ day2/wyjatki.py
@@ -0,0 +1,12 @@
+# wyjątki - błedy podczas wykonywania programu
+
+# print(5 / 0)
+
+try:
+    print(5 / 0)
+except ZeroDivisionError:
+    print("Nie dziel przez zero")
+
+print("Program nadal działa")
+# Nie dziel przez zero
+# Program nadal działą

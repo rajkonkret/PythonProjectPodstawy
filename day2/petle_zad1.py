0c9e1d12d7694ca1d63b919198f7d0fcb7a05321
radek <rajkonkret660@gmail.com>
2026-06-09 14:35:06 +0200

    [2 25] update petla.py

--- day2/petle_zad1.py
+++ day2/petle_zad1.py
@@ -62,5 +62,48 @@ print("Po zakonczeniu pętli")
 for i in range(-10, 0):
     print(i)
 
-for i in range(10, 0, -2): # (start, stop, krok)
+for i in range(10, 0, -2):  # (start, stop, krok)
     print(i)
+
+imiona = ['Sylwia', "Kamila", "Karolina", "Monika"]
+
+# wypisac elementy z listy jeden pod drugim
+
+for o in imiona:
+    print(o)
+# Sylwia
+# Kamila
+# Karolina
+# Monika
+
+# 0 Sylwia
+
+for o in imiona:
+    print(imiona.index(o), o)
+# 0 Sylwia
+# 1 Kamila
+# 2 Karolina
+# 3 Monika
+
+for i in range(len(imiona)):
+    print(i, imiona[i])
+
+# enumerate() - zwraca numer i element kolekcji
+for p in enumerate(imiona):
+    print(p)
+# (0, 'Sylwia') ->  0 Sylwia
+# (1, 'Kamila')
+# (2, 'Karolina')
+# (3, 'Monika')
+a, b = (0, 'Sylwia')
+print(a, b)
+
+for i, o in enumerate(imiona):
+    print(i, o)
+
+for i, o in enumerate(imiona, start=1):
+    print(i, o)
+# 1 Sylwia
+# 2 Kamila
+# 3 Karolina
+# 4 Monika

1b2ab1018476b1531c100a3b381a44492575e5d7
radek <rajkonkret660@gmail.com>
2026-06-09 12:25:59 +0200

    [2 14] update warunki.py

--- day2/warunki.py
+++ day2/warunki.py
@@ -56,8 +56,20 @@ if n > 3:
 if (n := len(a)) > 3:
     print(f"Długośc wynosi: {n}, więcej niz 3")
 
-
 # pobrac zarobki
 # jesli zarobki mniejsze od 10000 podatek 0
 # dla pozostałych podatek 90% (0.9)
 # wypisac obliczony podaek
+
+zarobki = int(input("Podaj zarobki:"))
+
+podatek = 0
+if zarobki < 10_000:
+    podatek = 0
+elif zarobki < 100_000:
+    podatek = 0.6
+else:
+    podatek = 0.9
+
+print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# podatek 20% dla przeziału 10000 do 39999

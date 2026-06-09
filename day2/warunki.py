205d793fc8458eafbdfc6b7d093a724c9fe894f1
radek <rajkonkret660@gmail.com>
2026-06-09 11:37:30 +0200

    [2 9] update warunki.py

---
+++ day2/warunki.py
@@ -0,0 +1,26 @@
+# instrukcja warunkowa
+
+# if
+# w zaleznosci od warunkuwykona jeden lub drugi blok programu
+#
+
+odp = True
+
+if odp: print("Test")  # Test
+
+if odp:
+    # blok programu wykonany gdy warunek True
+    print("Test")
+
+# debugger - narzedzie do wykonywania kodu krok po kroku
+# pułapki - miejsce gdzie program się zatrzyma
+
+# odp = False
+if odp:
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza częśc programu")

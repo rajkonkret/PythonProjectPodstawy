75778523706e030ab7a7991d26933f5d747c4cae
radek <rajkonkret660@gmail.com>
2026-06-09 14:05:05 +0200

    [2 22] update wyjatki.py

---
+++ day2/petle_zad1.py
@@ -0,0 +1,20 @@
+# pętla - możliwosc wykonania kodu wielokrotnie
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(20):
+    pass  # nic nie rób
+
+print(i)  # 19
+
+for _ in range(15):
+    print("Test podłoga")
+    # print(_)  # 14
+
+print("Wyjscie z pętli")
+
+for i in range(5):
+    print(i * 2)
+    print(i + 2)

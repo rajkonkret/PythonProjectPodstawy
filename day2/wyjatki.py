7fbb41c6f7cbbe0d92ade9f487ed07efce69a5ee
radek <rajkonkret660@gmail.com>
2026-06-09 13:58:21 +0200

    [2 21] update wyjatki.py

--- day2/wyjatki.py
+++ day2/wyjatki.py
@@ -3,10 +3,26 @@
 # print(5 / 0)
 
 try:
-    print(5 / 0)
+    # print(5 / 0)
+    # int('A')
+    # print(2 + "Ania")
+    # raise KeyError("Błąd klucza")
+    wynik = 90 / 3
 except ZeroDivisionError:
     print("Nie dziel przez zero")
+except ValueError:
+    print("Błąd wartości")
+except TypeError:
+    print("Błąd typu")
+except Exception as e:
+    print("Błąd:", e)  # Błąd: 'Błąd klucza'
+else:  # działa tylko gdy nie ma błedu
+    print("wynik:", wynik)
+finally:  # wykona się zawsze
+    print("Następny pacjent")
+
 
 print("Program nadal działa")
 # Nie dziel przez zero
 # Program nadal działą
+# print("wynik:", wynik)

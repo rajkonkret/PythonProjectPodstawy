72b672d2646a1194aa18cdd6072985cce5ed6d91
radek <rajkonkret660@gmail.com>
2026-06-10 11:27:57 +0200

    [3 10] update pliki_csv1.py

--- day3/pliki_csv1.py
+++ day3/pliki_csv1.py
@@ -25,3 +25,20 @@ with open(filename, "w", newline="") as f:
     csv_writer = csv.DictWriter(f, fieldnames=fields)
     csv_writer.writeheader()
     csv_writer.writerow(dict_name)
+
+products = [
+    {'sku': 1, "exp_date": 'today', "price": 200},
+    {'sku': 2, "exp_date": 'today', "price": 200},
+    {'sku': 3, "exp_date": 'tomorrow', "price": 200},
+    {'sku': 4, "exp_date": 'today', "price": 200},
+    {'sku': 5, "exp_date": 'tomorrow', "price": 200},
+]
+
+list_product = [key for key in products[0]]
+print(list_product)
+
+filename = "records_discount.csv"
+with open(filename, "w", newline="") as f:
+    csv_writer = csv.DictWriter(f, fieldnames=list_product, delimiter=";")
+    csv_writer.writeheader()
+    csv_writer.writerows(products)  # writerows - lista słowników

29426b38f24db60830a3671b053612c2f9fb5a97
radek <rajkonkret660@gmail.com>
2026-06-10 08:44:22 +0200

    [3 1] update petla.py

--- day2/petle_zad3.py
+++ day2/petle_zad3.py
@@ -24,15 +24,28 @@ while licznik < 10:
 # --------------------------------------------------
 # Komunikat 3 !!!
 
-password = input("Podaj hasło:")
-while password != "secret":  # != - różne
-    password = input("Podaj hasło:")
-# Podaj hasło:asadsad
-# Podaj hasło:asdadfasfa
-# Podaj hasło:dsadfadfd
-# Podaj hasło:secret
+# password = input("Podaj hasło:")
+# while password != "secret":  # != - różne
+#     password = input("Podaj hasło:")
+# # Podaj hasło:asadsad
+# # Podaj hasło:asdadfasfa
+# # Podaj hasło:dsadfadfd
+# # Podaj hasło:secret
+#
+# while (password := input("Podaj hasło:")) != "secret":
+#     pass
+# # Podaj hasło:secret
+# # Podaj hasło:secret
 
-while (password := input("Podaj hasło:")) != "secret":
-    pass
-# Podaj hasło:secret
-# Podaj hasło:secret
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+
+while 5 in my_list:
+    my_list.remove(5)
+
+print(my_list)  # [1, 2, 3, 4, 6]
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+print(dict.fromkeys(my_list))
+# {1: None, 5: None, 2: None, 3: None, 4: None, 6: None}
+
+print(list(dict.fromkeys(my_list)))  # [1, 5, 2, 3, 4, 6]
